083c3f973a24d68137681ccdd1dfe82f6ec85252
Adrian Abreu <alu0100601398@ull.edu.es>
2015-10-23 08:57:09 +0000

    Creados test de setup y to string

---
+++ test/tc_fraccionario.rb
@@ -0,0 +1,13 @@
+require 'lib/fraccionario'
+require 'test/unit'
+
+class TestFraccionario < Test::Unit::TestCase
+    def setup
+        @uno = Fraccionario.new(1,2)
+        @dos = Fraccionario.new(3,9)
+    end
+    
+    def test_simple
+        assert_equal("(1/2)"),@uno.to_s)
+        assert_equal("(3/9)"),@dos.to_s)
+    end
